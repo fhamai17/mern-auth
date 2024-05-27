9ada15613aef78bd39af293b1c800a15d661787d
fhamai17 <fhamai.khong@gmail.com>
2024-05-27 11:36:34 +0700

    complete upload image funtional

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,15 +1,69 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import { useSelector } from "react-redux";
+import { useRef, useState, useEffect } from "react";
+import { getDownloadURL, getStorage, ref, uploadBytesResumable } from "firebase/storage";
+import app from "../firebase";
 const Profile = () => {
-  const {currentUser} = useSelector(state => state.user)
-    return (
-    <div className='p-3 max-w-lg mx-auto'>
-      <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
+  const fileRef = useRef(null);
+  const [image, setImage] = useState(undefined);
+  const [imagePercent, setImagePercent] = useState(0);
+  const [imageError, setImageError] = useState(false);
+  const [formData, setFormData] = useState({});
+  const { currentUser } = useSelector((state) => state.user);
+  console.log(formData);
+  useEffect(() => {
+    if (image) {
+      handleFileUpload(image);
+    }
+  }, [image]);
+
+  const handleFileUpload = async (image) => {
+    console.log(image);
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + image.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, image);
+    uploadTask.on(
+      'state_changed',
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          setImagePercent(Math.round(progress));
+      },
+      (error)=>{
+        setImageError(true);
+      },
+      ()=>{
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL)=>{
+          console.log(downloadURL);
+          setFormData({...formData, profilePicture:downloadURL})
+        })
+      }
+    );
+  };
+
+  return (
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
       <form className="flex flex-col gap-4">
-        <img src={currentUser.profilePicture} alt="profile" 
-        className='h-24 w-24 self-center cursor-pointer rounded-full object-cover mt-2' />
         <input
-         defaultValue={currentUser.username}
+          type="file"
+          ref={fileRef}
+          hidden
+          accept="image/*"
+          onChange={(e) => setImage(e.target.files[0])}
+        />
+        <img
+          src={currentUser.profilePicture}
+          alt="profile"
+          className="h-24 w-24 self-center cursor-pointer rounded-full object-cover mt-2"
+          onClick={() => fileRef.current.click()}
+        />
+        <p className="text-sm self-center">{imageError ? (
+          <span className="text-red-700">Error uploading</span>
+        ) :  imagePercent > 0 && imagePercent < 100  ? (<span className="text-state-700">{`Uploading: ${imagePercent} %`}</span>) : 
+        imagePercent  === 100 ? (<span className="text-green-700">Image uploaded successful</span>) : ''}</p>
+        <input
+          defaultValue={currentUser.username}
           type="text"
           placeholder="Username"
           id="username"
@@ -28,15 +82,19 @@ const Profile = () => {
           id="password"
           className="bg-slate-100 p-3 rounded-lg"
         />
-        <button className='bg-slate-700 text-white p-3 rounded-lg uppercase
-        hover:opacity-95 disabled:opacity-80 '>update</button>
+        <button
+          className="bg-slate-700 text-white p-3 rounded-lg uppercase
+        hover:opacity-95 disabled:opacity-80 "
+        >
+          update
+        </button>
       </form>
-      <div className='flex justify-between mt-5'>
-        <span className='text-red-700'>Delete Account</span>
-        <span className='text-red-700'>Sign Out</span>
+      <div className="flex justify-between mt-5">
+        <span className="text-red-700">Delete Account</span>
+        <span className="text-red-700">Sign Out</span>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
